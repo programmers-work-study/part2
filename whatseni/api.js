535390f032d9d4e4a0003cf969929189ff487bd1
seeys <es2008e@naver.com>
2024-06-04 21:31:03 +0900

    api, 기본 파일 구조 작성

---
+++ whatseni/api.js
@@ -0,0 +1,17 @@
+const URL = 'https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev'
+const IMG_URL = `https://fe-dev-matching-2021-03-serverlessdeploymentbuck-1ooef0cg8h3vq.s3.ap-northeast-2.amazonaws.com/public`
+
+const fetchAPI = async (url) => {
+  try {
+    const response = await fetch(url, { method: 'GET' });
+    if (response) return response;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export const API = {
+  fetchRootList: () => { return fetchAPI(URL) },
+  fetchFolderList: () => { return fetchAPI(`${URL}/${folderID}`) },
+  fetchFileImage: () => { return fetchAPI(`${IMG_URL}/${filePath}`) }
+}
