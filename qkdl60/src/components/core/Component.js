7ea743ea9a8b7a11f0092c44a5c37999a61ac9b7
nocheol <qkdl60@gmail.com>
2024-06-10 22:26:05 +0900

    refactor : component 수정

--- qkdl60/src/components/core/Component.js
+++ qkdl60/src/components/core/Component.js
@@ -5,21 +5,30 @@ export default class Component {
   constructor({$target, props}) {
     this.$target = $target;
     this.props = props;
-
+    this.state = this.initState();
     this.render();
     this.setup();
+  }
+  initState() {
+    return {};
   }
   setState(nextValue) {
     this.state = nextValue;
     this.render();
   }
   render() {
+    //기본적으로 innerHTML으로
     this.$target.innerHTML = this.template();
     this.mounted();
   }
-  mounted() {}
-  setup() {}
+  mounted() {
+    //렌더링 후 필요한 작업들
+  }
+  setup() {
+    //처음 렌디링시 실행되는 것들, 이벤트 리스너 등
+  }
   template() {
+    //렌더링 할 내용
     return ``;
   }
 }
