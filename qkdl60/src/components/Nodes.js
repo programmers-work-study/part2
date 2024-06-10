d96f5f3359d7bbe97094e2070316a9e460bcb398
nocheol <qkdl60@gmail.com>
2024-06-10 22:27:44 +0900

    feat : Nodes 컴포넌트 추가

---
+++ qkdl60/src/components/Nodes.js
@@ -0,0 +1,18 @@
+import Component from "./core/Component.js";
+export default class Nodes extends Component {
+  template() {
+    return (
+      (this.props.isRoot
+        ? ""
+        : `<div class="Node prev">
+    <img src="assets/prev.png"/>
+    </div>`) + this.props.nodes.map((node) => nodeTemplate(node.type, node.name, node.id)).join("")
+    );
+  }
+}
+
+const nodeTemplate = (type, name, id) => `
+<div class="Node" id="${id}" data-type="${type}" data-name="${name}">
+  <img src="assets/${type === "FILE" ? "file" : "directory"}.png"/>
+  <div>${name}</div>
+</div>`;
